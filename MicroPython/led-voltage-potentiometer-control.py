11900f1367c8365b7aa093bbaaa9951bc924cd1c
Mario Agostinho <mariojgagostinho@gmail.com>
2025-07-24 01:33:09 +0100

    - Control LED voltage using potentiometer

---
+++ MicroPython/led-voltage-potentiometer-control.py
@@ -0,0 +1,17 @@
+from machine import Pin, PWM, ADC
+from time import sleep
+
+POT_PIN = 28
+LED_PIN = 15
+
+pot = ADC(POT_PIN)
+
+anout = PWM(Pin(LED_PIN))
+anout.freq(1000)
+anout.duty_u16(0)
+
+while True:
+    adc_value = pot.read_u16()
+    anout.duty_u16(int(adc_value))
+    
+    sleep(.1)
